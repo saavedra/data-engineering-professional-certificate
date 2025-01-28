8dc5404ef0483ee845dc638defbf8ba64dc363f2
Alexis Saavedra <hi@alexissaavedra.cl>
2025-01-28 20:56:27 +0100

    data modeling, transformation and serving

---
+++ 4. Data Modeling, Transformation, and Serving/lab 1.2 Data Modeling with DBT/classicmodels_modeling/models/star_schema/dim_offices.sql
@@ -0,0 +1,8 @@
+SELECT 
+    officeCode as office_key, 
+    postalCode as postal_code, 
+    city as city, 
+    state as state, 
+    country as country, 
+    territory as territory
+FROM classicmodels.offices
